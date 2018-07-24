475b8b4d7d4e935b8492a7835cb4850e5be3fb63
judiba <judioba@gmail.com>
2018-07-24 00:13:02 -0300

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -9,14 +9,14 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Trial SSH Account===="
+echo -e "====Conta SSH Teste===="
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,143"
 echo -e "Port Dropbear  : 80,444"
 echo -e "Port SSL/TLS   : 443"
 echo -e "Port Squid     : 8080,3128"
-echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
+echo -e "Config OpenVPN (TCP 1194): http://$IP:81/r3v1v3r.ovpn"
 echo -e "Username: $Login"
 echo -e "Password: $Pass\n"
 echo -e "========================="
-echo -e "~sivoi~"
+echo -e "R3V1V3R"
