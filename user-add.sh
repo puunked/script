05ad3ec97476c6a78f39af4f90f742018a519be6
judiba <judioba@gmail.com>
2018-07-24 15:23:51 -0300

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -2,14 +2,14 @@
 
 read -p "Username : " Login
 read -p "Password : " Pass
-read -p "Expired (hari): " masaaktif
+read -p "Expired (hoje): " masaaktif
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Informasi SSH Account===="
+echo -e "====Informação da Conta SSH===="
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,143"
 echo -e "Port Dropbear  : 80,444"
@@ -19,6 +19,6 @@ echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
-echo -e "Aktif Sampai: $exp"
+echo -e "Ativo até: $exp"
 echo -e "============================="
-echo -e "~sivoi~"
+echo -e "R3V1V3R"
