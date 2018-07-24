fa932797b5efc7df4429932a1fbf3d657d9cbb0b
judiba <judioba@gmail.com>
2018-07-24 15:33:45 -0300

    Update user-list.sh

--- user-list.sh
+++ user-list.sh
@@ -5,15 +5,15 @@ echo "USERNAME          EXP DATE     "
 echo "-------------------------------"
 while read expired
 do
-        AKUN="$(echo $expired | cut -d: -f1)"
+        CONTA="$(echo $expired | cut -d: -f1)"
         ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
-        exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+        exp="$(chage -l $CONTA | grep "Account expires" | awk -F": " '{print $2}')"
         if [[ $ID -ge 1000 ]]; then
-        printf "%-17s %2s\n" "$AKUN" "$exp"
+        printf "%-17s %2s\n" "$CONTA" "$exp"
         fi
 done < /etc/passwd
-JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+TOTAL="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "-------------------------------"
-echo "Jumlah akun: $JUMLAH user"
+echo "Número de contas: $TOTAL usuários"
 echo "-------------------------------"
-echo -e "~sivoi~"
+echo -e "R3V1V3R"
