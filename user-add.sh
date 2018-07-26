77ebcb89048e1257c43cb4c52df901edaaa0e900
judiba <judioba@gmail.com>
2018-07-26 15:46:34 -0300

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+cd
+adduser r3v1v3r sudo
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hoje): " masaaktif
@@ -12,10 +13,11 @@ echo -e ""
 echo -e "====Informação da Conta SSH===="
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,143"
-echo -e "Port Dropbear  : 80,444"
-echo -e "Port SSL/TLS   : 443"
-echo -e "Port Squid     : 8080,3128"
-echo -e "Config OpenVPN (TCP 1194): http://$IP:81/r3v1v3r.ovpn"
+echo -e "Port Dropbear  : 443,444"
+echo -e "Port SSL/TLS   : 993"
+echo -e "Port Squid     : 80,8080,3128"
+echo -e "Config OpenVPN R3V1V3R(TCP 1194): http://$IP:81/r3v1v3r.ovpn"
+echo -e "Config OpenVPN STUNNEL(TCP 1195): http://$IP:81/stunnel.ovpn"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
