93487a675b3fe05c0672f5c92ca855d2118d86f2
judiba <judioba@gmail.com>
2018-07-27 02:56:09 +0000

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+clear
 read -p "Username : " Login
 read -p "Password : " Pass
 read -p "Expired (hoje): " masaaktif
@@ -8,18 +8,22 @@ IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
-echo -e ""
-echo -e "====Informação da Conta SSH===="
+echo -e "\033[01;34m"
+echo -e "====Informação da Conta SSH====\n\033[01;35m"
 echo -e "Host: $IP" 
 echo -e "Port OpenSSH   : 22,143"
 echo -e "Port Dropbear  : 443,444"
-echo -e "Port SSL/TLS   : 993"
+echo -e "Port SSL/TLS   : 993,8888"
 echo -e "Port Squid     : 80,8080,3128"
-echo -e "Config OpenVPN R3V1V3R(TCP 1194): http://$IP:81/r3v1v3r.ovpn"
-echo -e "Config OpenVPN STUNNEL(TCP 1195): http://$IP:81/stunnel.ovpn"
+echo -e "\033[01;32m"
+echo -e "Config OpenVPN R3V1V3R (TCP 1194): http://$IP:81/r3v1v3r.ovpn"
+echo -e "Config OpenVPN STUNNEL (UDP 1195): http://$IP:81/stunnel.ovpn"
+echo -e "Config OpenVPN VIVO    (TCP 8090): http://$IP:81/vivo.ovpn"
+echo -e "\033[01;33m"
 echo -e "Username: $Login "
 echo -e "Password: $Pass"
-echo -e "-----------------------------"
-echo -e "Ativo até: $exp"
-echo -e "============================="
-echo -e "R3V1V3R"
+echo -e "\033[01;35m"
+echo -e "-----------------------------\033[01;32m"
+echo -e "Ativo até: $exp\033[01;35m"
+echo -e "=============================\n\033[01;34m"
+echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
