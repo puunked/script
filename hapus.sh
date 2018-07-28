4bbe1b87aab33aa1e862eea6e0164fac7e6a13ce
judiba <judioba@gmail.com>
2018-07-28 15:57:55 -0300

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -5,6 +5,10 @@ read -p "O nome do usuário do SSH será excluído : " User
 if getent passwd $User > /dev/null 2>&1; then
         userdel $User
         echo -e "Usuário $User foi excluído."
+        echo -e "=============================\n\033[01;34m"
+        echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
 else
         echo -e "FALHOU: Usuário $User está faltando."
+        echo -e "=============================\n\033[01;34m"
+        echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
 fi
