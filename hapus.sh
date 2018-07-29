0d88463e47c617858d466f64748d8ca10d99dfdd
judiba <judioba@gmail.com>
2018-07-29 10:32:19 -0300

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+verm="\033[1;31m"
+verd="\033[1;32m"
+resc="\e[0m"
+ 
+users_num=$(who | grep pts | wc -l)
+ 
+readarray -t users < <(who | awk '{print $1}' | uniq -d)
+for ((i=0;i<${#users[*]};i++)); do
+    if [[ ${users[$i]} != 'root' ]]; then
+        echo -e "Usuário: $verd${users[$i]}$resc conectado: $verm$(who | grep -c ${users[$i]}) vezes $resc"
+    fi
+ 
+echo -e "\nTotal: $verm$users_num$resc"
+
 read -p "O nome do usuário do SSH será excluído : " User
 
 if getent passwd $User > /dev/null 2>&1; then
@@ -12,3 +26,5 @@ else
         echo -e "=============================\n\033[01;34m"
         echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
 fi
+
+done
