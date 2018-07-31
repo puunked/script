82dd809c50094585ea7cf08c710fe26e2960d1f7
judiba <judioba@gmail.com>
2018-07-31 22:58:44 +0000

    Update hapus.sh

--- hapus.sh
+++ hapus.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
+clear
+echo -e "\033[0;31m"
+cut -d : -f 1,3 /etc/passwd | grep -A2000 1000
+echo -e "\033[1;33m"
 
-verm="\033[1;31m"
-verd="\033[1;32m"
-resc="\e[0m"
- 
-users_num=$(who | grep pts | wc -l)
- 
-readarray -t users < <(who | awk '{print $1}' | uniq -d)
-for ((i=0;i<${#users[*]};i++)); do
-    if [[ ${users[$i]} != 'root' ]]; then
-        echo -e "Usuário: $verd${users[$i]}$resc conectado: $verm$(who | grep -c ${users[$i]}) vezes $resc"
-    fi
- 
-echo -e "\nTotal: $verm$users_num$resc"
-
-read -p "O nome do usuário do SSH será excluído : " User
+read -p "O nome do usuário do SSH que será excluído : " User
 
 if getent passwd $User > /dev/null 2>&1; then
         userdel $User
@@ -26,5 +16,3 @@ else
         echo -e "=============================\n\033[01;34m"
         echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
 fi
-
-done
