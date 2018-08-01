5c6423df696b16490f3f263c1f7de9664989848d
judiba <judioba@gmail.com>
2018-08-01 01:52:37 +0000

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -3,17 +3,17 @@ clear
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo -e "\033[1;37m"
 clear
-tput setaf 8 ; tput setab 5 ; tput bold ; printf '%35s%s%-25s\n' "R3V1V3R" ; tput sgr0
+tput setaf 8 ; tput setab 5 ; tput bold ; printf '%25s%s%-15s\n' "R3V1V3R" ; tput sgr0
 echo ""
-echo -e "\033[01;34m                    R3V1V3R 1NT3RNET L1VR3      \033[1;33m"
+echo -e "\033[01;34m         R3V1V3R 1NT3RNET L1VR3      \033[1;33m"
 echo ""
-echo -e '\033[05;31m-------------------------------------------------------\033[00;37m'
-echo -e "[1]\033[01;32m•=> »INSTALAR O\033[0;35mVNC\033[1;33m"
+echo -e '\033[05;31m----------------------------------------\033[00;33m'
+echo -e "[1]\033[01;32m•=> » INSTALAR O\033[0;35m VNC\033[1;33m"
 echo ""
-echo -e "[2]\033[01;32m•=> »INICIAR O\033[0;35mVNC\033[1;33m"
+echo -e "[2]\033[01;32m•=> » INICIAR O\033[0;35m VNC\033[1;33m"
 echo ""
-echo -e "[3]\033[01;32m•=> »SAIR E VOLTAR AO\033[0;35mMENU\033[1;33m"
-echo -e '\033[05;31m-------------------------------------------------------\033[00;37m'
+echo -e "[3]\033[01;32m•=> » SAIR E VOLTAR AO\033[0;35m MENU\033[1;33m"
+echo -e '\033[05;31m----------------------------------------\033[00;33m'
 echo ""
 read -p "Selecione:[1-3]: " -e -i 1 resposta
 
@@ -24,16 +24,16 @@ clear
 fi
 
 if [[ "$resposta" = '2' ]]; then
+clear
 echo -e "\033[1;34m ESCOLHA UMA SENHA MINIMA DE 6 CARACTERES DEPOIS CONFIRME\033[1;32m"
 sleep 3
-
 vncserver
-
+netstat -nlpt | grep Xtightvnc
 echo -e "\033[1;34m VNC se conecta usando o IP da VPS na porta 5901\033[1;32m"
 echo -e "\033[1;34m Desta forma: $MYIP:5901\033[1;32m"
 echo -e "\033[1;34m Para acessar a interface gráfica\033[1;32m"
 echo -e "\033[1;34m Baixe apk ou programa do VNC VIWER\033[1;32m"
-echo -e "\033[1;34m Coloque o Ip e a porta 5901\033[1;32m"
+echo -e "\033[1;34m Coloque o IP da VPS e a porta 5901\033[1;32m"
 echo -e "\033[1;34m PRONTO PARA USAR.\033[1;32m"
 echo -e "=============================\n\033[01;34m"
 echo -e "R3V1V3R 1NT3RN3T L1VR3\033[01;33m"
